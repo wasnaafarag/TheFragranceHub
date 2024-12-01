7e6c466773b5a8edcbb5910687456fd4d19b8337
wasnaafarag <146942173+wasnaafarag@users.noreply.github.com>
2024-12-01 23:08:10 +0200

    .

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express'); //define what variable the express package is
 const server = express()
 const port = 7777 //port number 
-server.use(express)
+server.use(express.json())
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('data.db')
 
-server.get("/", (request, respond) => {
-    response.send("hello wasnaa!");
+server.get("/", (req, res) => {
+    res.send("hello wasnaa!");
 
 });
 
@@ -19,16 +19,33 @@ const createusertable = `CREATE TABLE IF NOT EXISTS user(
 )`
 
 //register
-server.post('/register', (request, respond) => {
+server.post('/register', (req, res) => {
     let name= req.body.name;
     let age= req.body.age;
     let email=req.body.email;
     let password= req.body.password;
-    db.run(`insert into user (name,age,email,password) values('${name}',${age}, '${email}', '${password}') `)
+    db.run(`insert into user (name,age,email,password) values('${name}',${age}, '${email}', '${password}')`, (err) =>{
+        if (err) {
+            console.log(err.message)
+            return res.send (err)
+        }
+        else
+        return res.send ('Welcome to The Fragrance Hub!')
+    })
 })
+
 
 
 server.listen(port, () => {
     console.log(`server is listening at port ${port}`)
+    db.serialize(() => {
+        db.exec(createusertable, (err)=>{
+            if (err) {
+                console.error('Error creating user table.', (err))
+            } else{
+                console.log('User table was created succesfully.')
+            }
+        })
+    })
 
 });
