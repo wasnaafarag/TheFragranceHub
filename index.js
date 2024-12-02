1fd33cb07f592d29ca6cb40af0cd510bf336440c
wasnaafarag <146942173+wasnaafarag@users.noreply.github.com>
2024-12-02 22:46:35 +0200

    .

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const createusertable = `CREATE TABLE IF NOT EXISTS user(
     age integer,
     email text not null,
     password text not null,
-    gender text
+    gender text not null
 )`
 
 const creatquestionstable = `CREATE TABLE IF NOT EXISTS questions(
@@ -28,9 +28,9 @@ const creatquestionstable = `CREATE TABLE IF NOT EXISTS questions(
 const createanswerstable = `CREATE TABLE IF NOT EXISTS answers(
     ID integer primary key autoincrement, 
     question_id int not null, 
-    foreign key (question_id) references questions(ID),
     answer text, 
-    preferred_scents text not null
+    preferred_scents text not null,
+    FOREIGN KEY (question_id) REFERENCES questions(ID)
 )`
 
 const createperfumestable = `CREATE TABLE IF NOT EXISTS perfumes(
@@ -45,100 +45,229 @@ const createperfumestable = `CREATE TABLE IF NOT EXISTS perfumes(
 const createrecommendationstable = `CREATE TABLE IF NOT EXISTS recommendations(
     ID integer primary key autoincrement, 
     user_id int not null,
-    foreign key (user_id) references user(ID),
     perfumes_id int not null,
-    foreign key (perfumes_id) references perfumes(ID)
+    FOREIGN KEY (user_id) REFERENCES user(ID),
+    FOREIGN KEY (perfumes_id) REFERENCES perfumes(ID)
 )`
 
 
-//register
+
 server.post('/register', (req, res) => {
-    let username= req.body.name;
-    let age= req.body.age;
-    let email=req.body.email;
-    let password= req.body.password;
-    db.run(`insert into user (username,age,email,password) values('${username}',${age}, '${email}', '${password}')`, (err) =>{
+    let username = req.body.username;
+    let age = req.body.age;
+    let email = req.body.email;
+    let password = req.body.password;
+    let gender = req.body.gender;
+    db.run(`insert into user (username,age,email,password,gender) values('${username}',${age}, '${email}', '${password}','${gender}')`, (err) => {
         if (err) {
             console.log(err.message)
-            return res.send (err)
+            return res.send(err)
         }
         else
-        return res.send ('Welcome to The Fragrance Hub!')
+            return res.send('Welcome to The Fragrance Hub!')
     })
 })
 
 server.post('/questions', (req, res) => {
-    let question= req.body.question;
-    let category= req.body.category;
-    db.run(`insert into questions (question, category) values('${question}','${category}')`, (err) =>{
+    let question = req.body.question;
+    let category = req.body.category;
+    db.run(`insert into questions (question, category) values('${question}','${category}')`, (err) => {
         if (err) {
             console.log(err.message)
-            return res.send (err)
+            return res.send(err)
         }
         else
-        return res.send ('Good job Wasnaa!')
+            return res.send('Good job Wasnaa!')
     })
 })
 
 
 server.post('/answers', (req, res) => {
-    let question_id= req.body.question_id;
-    let answer= req.body.answer;
-    let preferred_scents=req.body.preferred_scents;
-    db.run(`insert into answers (question_id,answer,preferred_scents) values(${question_id},'${answer}', '${preferred_scents}')`, (err) =>{
+    let question_id = req.body.question_id;
+    let answer = req.body.answer;
+    let preferred_scents = req.body.preferred_scents;
+    db.run(`insert into answers (question_id,answer,preferred_scents) values(${question_id},'${answer}', '${preferred_scents}')`, (err) => {
         if (err) {
             console.log(err.message)
-            return res.send (err)
+            return res.send(err)
         }
         else
-        return res.send ('Good job Sona!')
+            return res.send('Good job Sona!')
     })
 })
 
 
 server.post('/choosescents', (req, res) => {
-    let name= req.body.name;
-    let description= req.body.description;
-    let type=req.body.type;
-    let brand= req.body.brand;
-    let image= req.body.image;
-    db.run(`insert into perfumes (name,description,type,brand,image) values('${name}','${description}', '${type}', '${brand}','${image}')`, (err) =>{
+    let name = req.body.name;
+    let description = req.body.description;
+    let type = req.body.type;
+    let brand = req.body.brand;
+    let image = req.body.image;
+    db.run(`insert into perfumes (name,description,type,brand,image) values('${name}','${description}', '${type}', '${brand}','${image}')`, (err) => {
         if (err) {
             console.log(err.message)
-            return res.send (err)
+            return res.send(err)
         }
         else
-        return res.send ('Welcome to The Fragrance Hub!')
+            return res.send('Welcome to The Fragrance Hub!')
     })
 })
 
 
 server.post('/recommendations', (req, res) => {
-    let user_id= req.body.user_id;
-    let perfumes_id= req.body.perfumes_id;
-    db.run(`insert into recommendations (user_id,perfumes_id) values(${user_id},${perfumes_id})`, (err) =>{
+    let user_id = req.body.user_id;
+    let perfumes_id = req.body.perfumes_id;
+    db.run(`insert into recommendations (user_id,perfumes_id) values(${user_id},${perfumes_id})`, (err) => {
         if (err) {
             console.log(err.message)
-            return res.send (err)
+            return res.send(err)
         }
         else
-        return res.send ('Welcome to The Fragrance Hub!')
+            return res.send('Welcome to The Fragrance Hub!')
+    })
+})
+
+server.put('/register:ID/:password', (req, res) => {
+    db.run(`update user set password= '${req.params.password}' where ID = ${req.params.ID}`, (err) => {
+        if (err) {
+            console.log(err.message)
+            return res.send(err)
+        }
+        else
+            return res.send('Edited successfully!')
     })
 })
 
 
 
+server.get('/register', (req, res) => {
+    const register = `select * from user`
+    db.all(register, (err, rows) => {
+        if (err) {
+            console.log(err.message)
+            return res.send(err)
+        }
+        else
+            return res.json(rows)
+    })
+})
+
+
+server.get('/choosescents', (req, res) => {
+    const choosescents = `select * from choosescents`
+    if (description) {
+        choosescents += `where description = '${description}'`
+    }
+    if (type) {
+        if (description) {
+            choosescents += `and type = '${type}'`
+
+        }
+        else {
+            choosescents += `where type = '${type}'`
+        }
+
+    }
+
+    db.all(constregister, (err), rows => {
+        if (err) {
+            console.log(err.message)
+            return res.send(err)
+        }
+        else
+            return res.json(rows)
+    })
+})
+
+
+server.get('/questions', (req, res) => {
+    const questions = `select * from questions`
+    db.all(questions, (err, rows) => {
+        if (err) {
+            console.log(err.message)
+            return res.send(err)
+        }
+        else
+            return res.json(rows)
+    })
+})
+
+server.get('/answers', (req, res) => {
+    const answers = `select * from answers`
+    db.all(answers, (err, rows) => {
+        if (err) {
+            console.log(err.message)
+            return res.send(err)
+        }
+        else
+            return res.json(rows)
+    })
+})
+
+server.get ('/recommendations', (req, res) => {
+    const recommendations=`select * from recommendations`
+    db.all(recommendations, (err, rows) =>{
+        if (err) {
+            console.log(err.message)
+            return res.send(err)
+        }
+        else
+        return res.json(rows) 
+    })
+    })
+
+server.delete('/register/:ID', (req, res) => {
+    db.run(`delete from user where ID= ${request.params.ID}`, (err) => {
+        if (err) {
+            console.log(err.message)
+            return res.send(err)
+        }
+        else
+            return res.send('Deleted successfully!')
+    })
+})
+
 server.listen(port, () => {
     console.log(`server is listening at port ${port}`)
     db.serialize(() => {
-        db.exec(createusertable, (err)=>{
+        db.exec(createusertable, (err) => {
             if (err) {
                 console.error('Error creating user table.', (err))
-            } else{
+            } else {
                 console.log('User table was created succesfully.')
+            }
+        })
+        db.exec(creatquestionstable, (err) => {
+            if (err) {
+                console.error('Error creating user table.', (err))
+            } else {
+                console.log('Questions table was created succesfully.')
+            }
+        })
+        db.exec(createanswerstable, (err) => {
+            if (err) {
+                console.error('Error creating user table.', (err))
+            } else {
+                console.log('Answers table was created succesfully.')
+            }
+        })
+        db.exec(createperfumestable, (err) => {
+            if (err) {
+                console.error('Error creating user table.', (err))
+            } else {
+                console.log('Perfumes table was created succesfully.')
+            }
+        })
+        db.exec(createrecommendationstable, (err) => {
+            if (err) {
+                console.error('Error creating user table.', (err))
+            } else {
+                console.log('Recommendations table was created succesfully.')
             }
         })
     })
 
-});
+})
+
+
 
